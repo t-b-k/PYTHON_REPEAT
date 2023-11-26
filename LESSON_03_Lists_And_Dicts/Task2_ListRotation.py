c939463825b04b9c01ededcaad99d5c746c1aec9
Tatiana <t-b-k@bk.ru>
2023-11-26 17:58:39 +0300

    Tasks 1 and 2 from Deminar 3 are done

--- LESSON_03_Lists_And_Dicts/Task2_ListRotation.py
+++ LESSON_03_Lists_And_Dicts/Task2_ListRotation.py
@@ -4,4 +4,30 @@
 Необходимо сдвинуть последовательность на К элементов вправо. 
 Сдвиг циклический. 
 """
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Иcходный список = {}".format(list_1))
+k = int(input("На сколько элементов вправо будем сдвигать этот список? => "))
+k = k%len(list_1)
+print("k = {}".format(k))
+res = list([0])*10
+print("Новый список = {}".format(res))
+if k != 0 : 
+    for i in range(len(list_1)) : 
+        res[(k+i)%len(list_1)] = list_1[i]
+else : 
+    res = list(list_1)
+print("Итоговый список = {}".format(res))
 
+"""
+Вариант, разобранный на семинаре
+"""
+list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print("Иcходный список = {}".format(list_1))
+k = int(input("На сколько элементов вправо будем сдвигать этот список? => "))
+k = k%len(list_1)
+print("k = {}".format(k))
+res = list(list_1)
+for i in range(k): 
+    res.insert(0,res.pop(-1))
+print("Итоговый список = {}".format(res))
+    
