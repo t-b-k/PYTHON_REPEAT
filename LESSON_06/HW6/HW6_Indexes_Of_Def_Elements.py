03821d753f816908b4177fdb0f9b6343073db2fb
Tatiana <t-b-k@bk.ru>
2024-01-11 11:55:14 +0300

    Started doing the second task from HW6

---
+++ LESSON_06/HW6/HW6_Indexes_Of_Def_Elements.py
@@ -0,0 +1,16 @@
+# Задача 32. 
+# Определить индексы элементов массива (списка), значения которых 
+# принадлежат заданному диапазону (не меньше заданного минимума и не больше заданного максимума)
+# Ввод: [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, 9, 0, -5, -5, 7]
+#       7 10
+# Вывод: [1, 9, 13, 14, 19]
+
+numbers = input("Введите массив чисел в квадратных скобках и через запятую с пробелом => ")[1:-1].split(sep=", ")
+print(*numbers)
+minimum, maximum = input("Введите нижнюю и верхнюю границы диапазона через пробел => ").split()
+print(f"Минимум = {minimum}, максимум = {maximum}")
+for i in range(len(numbers)) : 
+    print(numbers[i], sep=" ")
+result = [i for i in range(len(numbers)) if minimum <= numbers[i] and numbers[i] <= maximum]
+print(len(result))
+print(result)
