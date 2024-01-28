39b10c8ad6438b481ccc4a9c4eb037015b0a1cd2
Tatiana <t-b-k@bk.ru>
2024-01-28 14:48:34 +0300

    Corrected seminar's solution

--- LESSON_07_Higher_Order_Functions/HW7/HW7_Vinnie_The_Pooh.py
+++ LESSON_07_Higher_Order_Functions/HW7/HW7_Vinnie_The_Pooh.py
@@ -30,26 +30,43 @@ def qty_of_syllables(phrase) :
      
 poem = "Союз-нерушимый-республик-сводобных Сплотила-навеки-великая-Русь-Мать"
 # poem = input("Покажите ваши стихи: \n")
-list_of_phrases = poem.split()
-print(list_of_phrases)
-is_poem = True
-for phrase in list_of_phrases : 
-    is_poem = is_poem and is_phrase(phrase)
-if is_poem :
-    set_of_syll_qties = set(map(qty_of_syllables, list_of_phrases))
-    if len(set_of_syll_qties) == 1 : 
-        print("Парам пам-пам!")
-    else : 
-        print("Пам парам!")
-else : 
-    print("Это вообще не стихи!")
+# list_of_phrases = poem.split()
+# print(list_of_phrases)
+# is_poem = True
+# for phrase in list_of_phrases : 
+#     is_poem = is_poem and is_phrase(phrase)
+# if is_poem :
+#     set_of_syll_qties = set(map(qty_of_syllables, list_of_phrases))
+#     if len(set_of_syll_qties) == 1 : 
+#         print("Парам пам-пам!")
+#     else : 
+#         print("Пам парам!")
+# else : 
+#     print("Это вообще не стихи!")
 
-# РЕШЕНИЕ В СТИЛЕ PYTHON (НА СЕМИНАРЕ)
+# РЕШЕНИЕ В СТИЛЕ PYTHON (НА СЕМИНАРЕ): работает неверно!
     
-alp = "аеёиоуыэюя"
-word_sug = input().split() # считали список фраз
-vowel_letters = [word.count(char) for word in word_sug for char in word if char.lower() in alp]
-if len(set(vowel_letters)) == 1 : 
+# alp = "аеёиоуыэюя"
+# word_sug = poem.split() # считали список фраз
+# print(word_sug)
+# vowel_letters = [word.count(char) for word in word_sug for char in word if char.lower() in alp]
+# print(vowel_letters)
+# print(len(set(vowel_letters)))
+# if len(set(vowel_letters)) == 1 : 
+#     print("Парам-пам-пам!")
+# else : 
+#     print("Пам-парам!")
+
+# РЕШЕНИЕ В СТИЛЕ PYTHON (НА СЕМИНАРЕ): исправленное!
+    
+dictionary = "аеёиоуыэюя"
+# poem = input().split()
+phrases = poem.split() # считали список фраз
+print(phrases)
+result = [sum([True for j in word if j.lower() in dictionary]) for word in phrases]
+print(result)
+
+if all(result) and len(set(result)) == 1 : # all позволяет исключить те стихи, в которых присутствуют фразы без гласных 
     print("Парам-пам-пам!")
 else : 
     print("Пам-парам!")
